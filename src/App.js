d7c5a1f5574e6b6aea1f9b1d828ae7f2c622bfd2
Rahul Verma <rahulverma1007199@gmail.com>
2023-04-30 10:08:15 +0000

    2 mistakes addewd

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import WhiteScreenProblem from "./Component/2WhiteScreenProblem";
+
+
+function App() {
+ 
+  return (
+    <div>
+      <WhiteScreenProblem/>
+    </div>
+  );
+}
+
+export default App;
