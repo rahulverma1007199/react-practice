6e580aa5ac1bfe3d5fab7ba70e881c69df0424d5
Rahul Verma <rahulverma1007199@gmail.com>
2023-05-25 02:24:41 +0000

    24-05 update

---
+++ src/Components/1.Installation.jsx
@@ -0,0 +1,11 @@
+
+    // as you know based on the new documation on 14/5/23, many people complained that create-react-app is too slow and outdated. 
+    // after the documentaton they suggest a framework than using a pure react library, npx create-react-app is a next.js command.
+    // and this states the vision of future react is server side rendering. So for begineers it should be important to know that why they are using this thing over that and if they have a reason to jump then they can but make sure to build some project on that.
+    // Is react dying -- NO
+    // Now what to choose
+    //  you won't be using react router dom but instead you will use app directory to create folder structure to define routes
+    // other difference is rendering components and pages -- so if you have an highly interactive website use CSR (client side rendering) if not then use SSR (Server side rendering) -- as SSR sends the render output which make website ranking great.
+
+    // Also to know what is trending go to https://2022.stateofjs.com/en-US/
+    // Compare react with other, check front-end frameworks, rendering framework and more. to get the overall idea of future technology.
---
+++ src/Components/2.DashBoard.jsx
@@ -0,0 +1,19 @@
+//what i learned from React Admin Dashboard project
+
+// Always start with what and how many page you need in your website -- like login,home, user etc.
+
+// why use jsx extension -- it's easy to handle html elements inside this extension
+
+// always see how to use react-router-dom by their update documentation -- as it gets updated quite frequently
+
+// we can create nested routes, see the users route for more.
+
+// after setting up the routes, make sure to analyse common componet and create them, eg -- navbar, sidebar, etc.
+
+// in html -- try to divide everthing in div or similar level, for better understanding and easy css.
+
+// include atleast div,span or any other element inside <li> tag.
+
+// <li> tag has it's own margin and to prevent this make m0,p0 in ul
+
+// for icons, table and more -- checkout material UI -- we can change the color of icons
