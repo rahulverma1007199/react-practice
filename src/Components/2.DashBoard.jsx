a5bcc7132c4b5b4aebc740de8a1631bfc7202b9b
Rahul Verma <rahulverma1007199@gmail.com>
2023-05-26 02:45:40 +0000

    dash

--- src/Components/2.DashBoard.jsx
+++ src/Components/2.DashBoard.jsx
@@ -17,3 +17,21 @@
 // <li> tag has it's own margin and to prevent this make m0,p0 in ul
 
 // for icons, table and more -- checkout material UI -- we can change the color of icons
+
+// Use &:nth-child(<index of child>) for advance css -- check it in sidebar .bottom
+
+// try using new div for padding and margin -- rather than distroying the main div.
+
+// give background colour transprent in case we are using dark and white mode.
+
+// scss -- &::placeholder
+
+// margin-right on df children is equal to giving gap on df div.
+
+// Sometimes it's better to give flex:<like 1> than to apply width, reference to .widget class. aka using flex is way better than custom widget.
+
+// refer to 'box shadow generator'
+
+// Width: max-content is used when like border is overflowing, so this make sure ki jitna content utna he rahega.
+
+// <div className='test test1'> to access test1 in scss -- .test{ &.test1{<assign property>}}
