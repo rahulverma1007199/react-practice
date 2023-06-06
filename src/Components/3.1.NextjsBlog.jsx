1640b300eff842d065d5e7948acbf3958cff374c
unknown <rahulverma1007199@gmail.com>
2023-06-06 10:06:16 +0530

    10 mint to course

--- src/Components/3.1.NextjsBlog.jsx
+++ src/Components/3.1.NextjsBlog.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+// Why do we need Next.js
 
-const NextjsBlog = () => {
-  return <div> NextjsBlog</div>;
-};
+// Biggest framework of React that is used to build Server Rendered Application
 
-export default NextjsBlog;
+// We can create all the page using single pages and it's really easy and group pages in next.js thus we are not dependent on react-router-dom library
+
+// for fullstack application we can create our own APIs and in react we have to use a framework like express.js and create a different appliation
+
+// with next js we can create server side and client side component together, so we can fetch data on server side and and improve the seo of application and client side component to intract with users
+
+// Next js is great for optimisation as it uses code spliting to improve application initial load time and automatically optmise the images by compressing them.
+
+// open next.js website and head to installation and run "npx create-next-app@latest ."
+
+// here we no to TypeScript, yes to ESLint (help us to find potential errors), no tailwind, yes to src. yes to app router, yes to alias
+
+// to run -- npm run dev
+
+// .next stores cache data
+
+// delete .svg files in public folder and we place our photos, icons in this folder
+
+// So every page we create will locate in the app folder and this folder represent the homepage and to create different pages just create different folder within and to create subpages, create folder in parents, like a tree with different subtress, but this is static, what about dynamic names, just like -- [anything like id or slug]
+
+// if you have multiple folder in single and want to group them, then use (like auth) in a folder to group, it's just for groiping and does not effect on routing
+
+// create folder and name the file page.jsx, how ever change the method name.
+
+// layout and components -- in layout we generally add common components and we can create different layout for different routes and we can create individual layout inside just add to that folder layout.jsx file.
+
+// as for page we are bound to use page.jsx but for component we can name anything to file.
